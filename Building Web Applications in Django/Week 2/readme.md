e14646a1a2253e64d4df6cd9a4722cc3856b2a91
Gabriel Reira <gabrielmfreira@gmail.com>
2021-07-16 11:45:08 -0300

    BWAD -  Week 2

---
+++ Building Web Applications in Django/Week 2/readme.md
@@ -0,0 +1,11 @@
+This week I learned more about views, built-in Django template tags and filters, Django shortcuts, how to apply Django's model query to retrieve items, and I also applied code of Django Template Language (DLT).
+
+Reference sources:
+
+- https://docs.djangoproject.com/en/3.0/topics/http/urls/
+- https://docs.djangoproject.com/en/3.0/topics/templates/
+- https://docs.djangoproject.com/en/3.0/ref/templates/language/
+- https://docs.djangoproject.com/en/3.0/ref/templates/builtins/
+- https://docs.djangoproject.com/en/3.0/intro/tutorial03/
+
+All the code was used in my [PythonAnywhere](https://www.pythonanywhere.com) environment. This activity starts where last week's activity let off.
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,26 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. 99932f26. Cheers!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:7]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def owner(request):
+    return HttpResponse("Hello, world. 21fb6e83 is the polls index. Cheers!")
