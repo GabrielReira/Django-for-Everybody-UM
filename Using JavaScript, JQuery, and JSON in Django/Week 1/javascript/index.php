77dbe081436c1aa39c5a3a571911047a48779f50
Gabriel Reira <gabrielmfreira@gmail.com>
2021-08-04 11:30:30 -0300

    UJJJ - Week 1

---
+++ Using JavaScript, JQuery, and JSON in Django/Week 1/javascript/index.php
@@ -0,0 +1,26 @@
+<p>View developer console for these examples.</p>
+<ul>
+    <li><a href="js-01.htm">js-01.htm</a> - document.write()</li>
+    <li><a href="js-02.htm">js-02.htm</a> - alert()</li>
+    <li><a href="js-03.htm">js-03.htm</a> - onclick=</li>
+    <li><a href="js-04.htm">js-04.htm</a> - src=</li>
+    <li><a href="js-05.htm">js-05.htm</a> - Syntax errors</li>
+    <li><a href="js-06.htm">js-06.htm</a> - console.log()</li>
+    <li><a href="js-07.htm">js-07.htm</a> - white space</li>
+    <li><a href="js-08.htm">js-08.htm</a> - new lines</li>
+</ul>
+<ul>
+    <li><a href="js-09.htm">js-09.htm</a> - functions</li>
+    <li><a href="js-10.htm">js-10.htm</a> - global scope</li>
+    <li><a href="js-11.htm">js-11.htm</a> - var - local scope</li>
+</ul>
+<ul>
+    <li><a href="js-12.htm">js-12.htm</a> - conditional (if)</li>
+    <li><a href="js-13.htm">js-13.htm</a> - multi-way if</li>
+    <li><a href="js-14.htm">js-14.htm</a> - while loop </li>
+    <li><a href="js-15.htm">js-15.htm</a> - object "associative array"</li>
+    <li><a href="js-16.htm">js-16.htm</a> - Counted loop</li>
+    <li><a href="js-17.htm">js-17.htm</a> - Loop and break</li>
+    <li><a href="js-18.htm">js-18.htm</a> - Loop and continue</li>
+    <li><a href="js-19.htm">js-19.htm</a> - Try / catch / finally</li>
+</ul>
---
+++ Using JavaScript, JQuery, and JSON in Django/Week 1/readme.md
@@ -0,0 +1,7 @@
+This week I studied JavaScript, it was like a review. I reviewed how to write syntactically correct JavaScript language, its debugging capabilities, how it works in the browser, and JS data types, functions, arrays, control structures, conditional, loops and errors.
+
+Reference sources:
+
+- https://developer.mozilla.org/en-US/docs/Learn/JavaScript
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript
+- https://www.w3schools.com/js/
